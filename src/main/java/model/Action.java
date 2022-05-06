90829c4e35e116addb9cf9061d507e789fc10c7a
KowalczykEmil <pomocyakoshi@gmail.com>
2022-05-06 20:06:24 +0200

    Created model packace - Logic

---
+++ src/main/java/model/Action.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface Action {
+    public double action(double x, double y);
+}
---
+++ src/main/java/model/Series.java
@@ -0,0 +1,6 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Series extends ArrayList<Stats> {
+}
